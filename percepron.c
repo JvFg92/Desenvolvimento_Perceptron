c49ad616f79db52a83fad55a37c88c60e8df0c8d
JvFg92 <joaovfg34@gmail.com>
2025-05-22 17:44:10 -0300

    v0-perceptron

--- percepron.c
+++ percepron.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief Computes the weighted sum of inputs for a perceptron.
+ *
+ * @param x Pointer to the input vector.
+ * @param w Pointer to the weight vector.
+ * @return double The weighted sum of the inputs.
+ */
 
+double perceptron(double *x, double *w){
+  double sum = 0.0;
+  for (int i = 0; i < 3; i++) {
+    sum += x[i] * w[i];
+  }
+  return sum;
+}
 
 int main(){
 
